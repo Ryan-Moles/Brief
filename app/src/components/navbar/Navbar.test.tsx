ee3564208488dbf4de5a28ad7d7664acfc539f6a
Ryan Moles <ryan.moles@appivate.mod.uk>
2025-02-07 11:57:13 +0000

    test

---
+++ app/src/components/navbar/Navbar.test.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it, vi } from "vitest";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+describe("test", () => {
+  it("will pass", async () => {
+    expect(1).toEqual(1);
+  });
+});
